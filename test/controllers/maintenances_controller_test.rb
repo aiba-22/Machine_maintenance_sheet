1b41f58ac9d0eb4e885ed683d1301f3efcf312dd
aiba <jazzarubamu@gmail.com>
2022-04-10 18:29:30 +0900

    add:メンテナンス一覧画面の作成

---
+++ test/controllers/maintenances_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class MaintenancesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get maintenances_index_url
+    assert_response :success
+  end
+
+end
